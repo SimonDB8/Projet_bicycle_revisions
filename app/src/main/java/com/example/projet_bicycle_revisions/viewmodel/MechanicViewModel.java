66ba2e71847939b4043781186f3ca327cc522b50
simon08 <simon.deblasi@students.hevs.ch>
2022-03-21 14:26:21 +0100

    MechanicViewModel

---
+++ app/src/main/java/com/example/projet_bicycle_revisions/viewmodel/MechanicViewModel.java
@@ -0,0 +1,78 @@
+package com.example.projet_bicycle_revisions.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.projet_bicycle_revisions.BaseApp;
+import com.example.projet_bicycle_revisions.database.entity.MechanicEntity;
+import com.example.projet_bicycle_revisions.database.repository.MechanicRepository;
+import com.example.projet_bicycle_revisions.util.OnAsyncEventListener;
+
+public class MechanicViewModel extends AndroidViewModel {
+
+    private MechanicRepository repository;
+
+    private Application application;
+
+    private final MediatorLiveData<MechanicEntity> observableMechanic;
+
+    public MechanicViewModel(@NonNull Application application, final String email, MechanicRepository mechanicRepository ) {
+        super(application);
+
+        this.application = application;
+        repository= mechanicRepository;
+        observableMechanic = new MediatorLiveData<>();
+        observableMechanic.setValue(null);
+
+        LiveData<MechanicEntity> mechanic = repository.getMechanic(email,application);
+        observableMechanic.addSource(mechanic, observableMechanic::setValue);
+
+    }
+
+    public static class Factory extends ViewModelProvider.NewInstanceFactory {
+
+        @NonNull
+        private final Application application;
+
+        private final String email;
+
+        private final MechanicRepository repository;
+
+        public Factory(@NonNull Application application, String email) {
+            this.application = application;
+            this.email = email;
+            repository = ((BaseApp) application).getMechanicRepository();
+        }
+
+        @Override
+        public <T extends ViewModel> T create(Class<T> modelClass) {
+            //noinspection unchecked
+            return (T) new MechanicViewModel(application, email, repository);
+        }
+    }
+
+        public LiveData<MechanicEntity> getClient() {
+            return observableMechanic;
+        }
+
+        public void createMechanic(MechanicEntity mechanic, OnAsyncEventListener callback) {
+            repository.insert(mechanic, callback, application);
+        }
+
+        public void updateClient(MechanicEntity mechanic, OnAsyncEventListener callback) {
+            repository.update(mechanic, callback, application);
+        }
+
+        public void deleteClient(MechanicEntity mechanic, OnAsyncEventListener callback) {
+            repository.delete(mechanic, callback, application);
+
+
+
+    }
+}
